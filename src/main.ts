8ba654b90f082a0da1e81c677326ede65db7846e
Keonho Kim <keonhokim@Keonhoui-MacBookAir.local>
2021-05-15 16:02:47 +0900

    socket.io test

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SocketIoAdapter } from './socket-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3001);
-  //app.useWebSocketAdapter(new SocketIoAdapter(app, corsOrigins));
 }
 bootstrap();
